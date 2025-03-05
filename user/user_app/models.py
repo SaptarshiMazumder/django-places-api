294914123f5ce77cf0a5a1c7c269f9776f786333
SaptarshiMazumder <48704857+SaptarshiMazumder@users.noreply.github.com>
2025-03-05 22:23:56 +0900

    made a manage.py file for users

--- user/user_app/models.py
+++ user/user_app/models.py
@@ -3,7 +3,22 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     # Add any additional fields here
-    pass
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_name="user_app_user_set",
+        related_query_name="user_app_user",
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name="user_app_user_set",
+        related_query_name="user_app_user",
+    )
 
     def __str__(self):
         return self.username
--- user/user_app/views.py
+++ user/user_app/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, permissions
 from user_app.serializers import UserSerializer
+from user_app.models import User
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
